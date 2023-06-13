58da041d3eae7a1504e7f9dd620026fd111b112e
Jorge Silva <jorgelfsilva5@gmail.com>
2023-06-13 15:10:46 -0300

    commit para teste de deploy

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,40 +4,44 @@ import { ReactComponent as Burger } from '../../assets/svg/list.svg'
 import { ReactComponent as Close } from '../../assets/svg/x-lg.svg'
 import { Link } from 'react-router-dom'
 
-const Sidebar = ({classe}) => {
-  /* console.log(classe.current); */
+const Sidebar = () => {
   const [isActive, setIsActive] = React.useState(false)
 
   const handleClick = e =>{
     setIsActive(current => !current)
   }
+  const height_screen = window.innerHeight  
 
-  React.useEffect(() =>{
-    
-  }, [isActive])
+  React.useEffect(() => {
+    if(isActive){
+      window.document.body.classList.add('locked')
+    }else if(isActive == null){
+      window.document.body.classList.remove('locked')
+    }
+  })
 
   return (
     <>
       <div className={style.burger} onClick={handleClick}>
         {isActive ? 
-          <div className={style.close_button}>
+          <div className={style.button_close}>
             <Close/> 
-          </div>:
-          <div className={style.open_button}>
-            <Burger/>
-          </div>}
+          </div>
+          : <Burger/>}
       </div>
       
       {isActive && 
       
-      <div className={`${style.menu} ${isActive ? 'active' : ''}`}>
-        <nav>
-          <Link style={{animationDelay: '.1s'}} to="#home">home</Link>
-          <Link style={{animationDelay: '.2s'}} to="#competencias">competências</Link>
-          <Link style={{animationDelay: '.3s'}} to="#experiencia">experiencia</Link> 
-          <Link style={{animationDelay: '.4s'}} to="#contato">contato</Link> 
-        </nav>
-      </div>
+      <section className={`${isActive ? 'active' : ''}`}>
+        <div className={style.menu_position}>
+          <nav className={style.menu} style={{height: `${height_screen}px`}}>
+            <Link style={{animationDelay: '.1s'}} to="#home">home</Link>
+            <Link style={{animationDelay: '.2s'}} to="#competencias">competências</Link>
+            <Link style={{animationDelay: '.3s'}} to="#experiencia">experiencia</Link> 
+            <Link style={{animationDelay: '.4s'}} to="#contato">contato</Link> 
+          </nav>
+        </div>
+      </section>
       }
     </>
   )
